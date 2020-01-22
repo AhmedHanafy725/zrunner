f1ef8c6e16e541bc3dfc3a640da99f0d8cc593c4
ahmedhanafy725 <ahmed.hanafy725@gmail.com>
2020-01-22 15:37:47 +0000

    Use jsx logger

--- test_runner.py
+++ test_runner.py
@@ -1,9 +1,10 @@
-from Jumpscale import j
-from importlib import import_module, sys
 import os
 import re
 import traceback
 import types
+from importlib import import_module, sys
+
+from Jumpscale import j
 
 VALID_TEST_NAME = re.compile("(?:^|[\b_\./-])[Tt]est")
 
@@ -12,19 +13,22 @@ class Skip(Exception):
     """Raise for skip test"""
 
 
-def skip(msg):
-    def dec(func):
-        def wrapper(*args, **kwargs):
-            raise Skip(msg)
+class TestTools:
+    @staticmethod
+    def skip(msg):
+        def dec(func):
+            def wrapper(*args, **kwargs):
+                raise Skip(msg)
 
-        wrapper.__test_skip__ = True
-        return wrapper
+            wrapper.__test_skip__ = True
+            return wrapper
 
-    return dec
+        return dec
 
+    def run(self, path=""):
+        if hasattr(self, "_dirpath") and not path:
+            path = j.sal.fs.joinPaths(self._dirpath, "tests")
 
-class TestTool:
-    def run(self, path):
         if not j.sal.fs.isAbsolute(path):
             path = j.sal.fs.joinPaths(j.sal.fs.getcwd(), path)
 
@@ -37,7 +41,8 @@ class TestTool:
                 self._run(test_name)
         else:
             self.discover(path)
-            self._run()
+            return self._run()
+        return 0
 
     def discover(self, path, test_name=""):
         self.modules = []
@@ -84,7 +89,7 @@ class TestTool:
             raise AttributeError(f"Test {test_name} is not found")
 
     def _run(self, test_name=""):
-        self.results = {"summary": {"passed": 0, "failures": 0, "errors": 0, "skips": 0}, "testcases": []}
+        self.results = {"summary": {"passes": 0, "failures": 0, "errors": 0, "skips": 0}, "testcases": []}
         for module in self.modules:
             self.before_all(module)
             if test_name:
@@ -95,8 +100,9 @@ class TestTool:
                         self.run_test(method, module)
 
             self.after_all(module)
-
         self.report()
+        if (self.results["summary"]["failures"] > 0) or (self.results["summary"]["errors"] > 0):
+            return 1
 
     def before_all(self, module):
         module_name = module.__file__
@@ -104,9 +110,8 @@ class TestTool:
             before_all = getattr(module, "before_all")
             try:
                 before_all()
-            except:
-                trace_back = traceback.format_exc()
-                self.add_helper_error(module_name, trace_back)
+            except Exception as error:
+                self.add_helper_error(module_name, error)
                 print("error\n")
 
     def after_all(self, module):
@@ -115,17 +120,14 @@ class TestTool:
             after_all = getattr(module, "after_all")
             try:
                 after_all()
-            except:
-                trace_back = traceback.format_exc()
-                self.add_helper_error(module_name, trace_back)
+            except Exception as error:
+                self.add_helper_error(module_name, error)
                 print("error\n")
 
     def before(self, module):
         if hasattr(module, "before"):
             before = getattr(module, "before")
-        else:
-            before = self.pass_method
-        return before
+            before()
 
     def after(self, module, test_name):
         module_name = f"{module.__file__}:{test_name}"
@@ -133,95 +135,107 @@ class TestTool:
             after = getattr(module, "after")
             try:
                 after()
-            except:
-                trace_back = traceback.format_exc()
-                self.add_helper_error(module_name, trace_back)
+            except Exception as error:
+                self.add_helper_error(module_name, error)
                 print("error\n")
 
     def run_test(self, method, module):
         module_name = module.__file__
         test_name = f"{module_name}:{method}"
-        before = self.before(module)
         try:
             test = getattr(module, method)
             if type(test) is not types.FunctionType:
                 return
             print(test_name, "...")
             if not self._is_skipped(test):
-                before()
+                self.before(module)
 
             test()
             self.add_success(test_name)
             print("ok\n")
-        except AssertionError:
-            trace_back = traceback.format_exc()
-            self.add_failure(test_name, trace_back)
+        except AssertionError as error:
+            self.add_failure(test_name, error)
             print("fail\n")
         except Skip as sk:
             skip_msg = f"SkipTest: {sk.args[0]}\n"
             self.add_skip(test_name, skip_msg)
             print("skip\n")
-        except Exception:
-            trace_back = traceback.format_exc()
-            self.add_error(test_name, trace_back)
+        except Exception as error:
+            self.add_error(test_name, error)
             print("error\n")
 
         self.after(module, test_name)
 
     def report(self):
         length = 70
-        for testcase in self.results["testcases"]:
-            if testcase.get("details"):
-                print("=" * length)
-                print(f'{testcase["status"].upper()}:', testcase["name"])
-                print("-" * length)
-                print(testcase["details"])
+        for result in self.results["testcases"]:
+            msg = result["msg"].split(": ")[1]
+            print(msg)
+            error = result["error"].split(": ")[1]
+            print(error)
 
         print("-" * length)
         all_tests = sum(self.results["summary"].values())
-        print(f"Ran {all_tests}\n\n")
-        print(
-            "{failed} Failed, {error} Error, {passed} Passed, {skip} Skip".format(
-                failed=self.results["summary"]["failures"],
-                error=self.results["summary"]["errors"],
-                passed=self.results["summary"]["passed"],
-                skip=self.results["summary"]["skips"],
+        print(f"Ran {all_tests} tests\n\n")
+        result_log = j.core.tools.log(
+            "{RED}%s Failed, {YELLOW}%s Errored, {GREEN}%s Passed, {BLUE}%s Skipped"
+            % (
+                self.results["summary"]["failures"],
+                self.results["summary"]["errors"],
+                self.results["summary"]["passes"],
+                self.results["summary"]["skips"],
             )
         )
-
-    def pass_method(self):
-        pass
+        result_str = j.core.tools.log2str(result_log)
+        print(result_str.split(": ")[1], "\u001b[0m")
 
     def _is_skipped(self, test):
         if hasattr(test, "__test_skip__"):
             return getattr(test, "__test_skip__")
 
     def add_success(self, test_name):
-        self.results["summary"]["passed"] += 1
-        result = {"name": test_name, "status": 'Passed'}
-        self.results["testcases"].append(result)
+        self.results["summary"]["passes"] += 1
 
-    def add_failure(self, test_name, trace_back):
+    def add_failure(self, test_name, error):
         self.results["summary"]["failures"] += 1
-        result = {"name": test_name, "status": 'Failed', "details": trace_back}
+        length = len(test_name) + 6
+        msg = "\n" + "=" * length + "\n" + "FAIL: " + test_name + "\n" + "-" * length + "\n"
+        log_msg = j.core.tools.log("{RED} %s " % msg, stdout=False)
+        str_msg = j.core.tools.log2str(log_msg)
+        log_error = j.core.tools.log("", exception=error, stdout=False)
+        str_error = j.core.tools.log2str(log_error)
+        result = {"msg": str_msg, "error": str_error}
         self.results["testcases"].append(result)
 
-    def add_error(self, test_name, trace_back):
+    def add_error(self, test_name, error):
         self.results["summary"]["errors"] += 1
-        result = {"name": test_name, "status": 'Error', "details": trace_back}
+        length = len(test_name) + 7
+        msg = "\n" + "=" * length + "\n" + "ERROR: " + test_name + "\n" + "-" * length + "\n "
+        log_msg = j.core.tools.log("{YELLOW} %s " % msg, stdout=False)
+        str_msg = j.core.tools.log2str(log_msg)
+        log_error = j.core.tools.log("", exception=error, stdout=False)
+        str_error = j.core.tools.log2str(log_error)
+        result = {"msg": str_msg, "error": str_error}
         self.results["testcases"].append(result)
 
     def add_skip(self, test_name, skip_msg):
         self.results["summary"]["skips"] += 1
-        result = {"name": test_name, "status": 'Skip', "details": skip_msg}
+        length = len(test_name) + 6
+        msg = "\n" + "=" * length + "\n" + "SKIP: " + test_name + "\n" + "-" * length + "\n "
+        log_msg = j.core.tools.log("{BLUE} %s " % msg, stdout=False)
+        str_msg = j.core.tools.log2str(log_msg)
+        skip_msg = "\n" + skip_msg + "\n"
+        log_skip = j.core.tools.log("{BLUE} %s " % skip_msg, stdout=False)
+        str_skip = j.core.tools.log2str(log_skip)
+        result = {"msg": str_msg, "error": str_skip}
         self.results["testcases"].append(result)
 
-    def add_helper_error(self, test_name, trace_back):
-        result = {"name": test_name, "status": 'Error', "details": trace_back}
+    def add_helper_error(self, test_name, error):
+        length = len(test_name) + 7
+        msg = "\n" + "=" * length + "\n" + "ERROR: " + test_name + "\n" + "-" * length + "\n "
+        log_msg = j.core.tools.log("{YELLOW} %s " % msg, stdout=False)
+        str_msg = j.core.tools.log2str(log_msg)
+        log_error = j.core.tools.log("", exception=error, stdout=False)
+        str_error = j.core.tools.log2str(log_error)
+        result = {"msg": str_msg, "error": str_error}
         self.results["testcases"].append(result)
-
-
-if __name__ == "__main__":
-    l = TestTool()
-    path = sys.argv[1]
-    l.run(path="/sandbox/code/github/threefoldtech/jumpscaleX_libs/test")
