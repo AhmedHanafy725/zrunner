8b9cc4551e6ef92efc3defff3c6b463c2678d728
ahmedhanafy725 <hanafya@incubaid.com>
2022-08-28 01:07:59 +0200

    Make runner standOFalone

---
+++ run.py
@@ -0,0 +1,4 @@
+from test_runner import TestTools
+
+t = TestTools()
+t.run_from_path("/sandbox/code/github/threefoldtech/test_runner/test.py")
---
+++ test.py
@@ -0,0 +1,23 @@
+from test_runner import skip
+
+def before():
+    print("before")
+
+def after():
+    print("after")
+
+
+def test1():
+    x = 5
+    assert x == 5
+
+def test2():
+    x = 4
+    assert x == 5
+
+def test3():
+    raise Exception("test")
+
+@skip("this skip test")
+def test4():
+    x = 5
